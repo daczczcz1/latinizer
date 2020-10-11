4eb80814b525565fc8ff9c947a3073b5993185ea
LAPTOP-QR8NNK6N\ja <daczosnek@gmail.com>
2020-10-11 23:15:00 +0200

    push to main

---
+++ README.md
@@ -0,0 +1,8 @@
+Dependencies: 
+- python 3.7
+- libs: librosa, madmom, numpy, soundfile (all installable with pip)
+
+Usage:
+```bash
+python latinizer.py [path_to_file_to_be_transformed]
+```
--- latinizer.py
+++ latinizer.py
@@ -3,38 +3,38 @@ import soundfile as sf
 import numpy as np
 import librosa
 import os
+import sys
 
-# noinspection SpellCheckingInspection
-filename = "/sourcewavs/bach1short"
-meter = 4
-# noinspection SpellCheckingInspection
-dirname = os.path.dirname(__file__)
-PATH = dirname + filename + ".wav"
-x, sr = librosa.load(PATH)
-
-proc = madmom.features.beats.DBNBeatTrackingProcessor(fps=100)
-act = madmom.features.beats.RNNBeatProcessor()(PATH)
-beat_times = proc(act)
-
-# we need to skip the last bar as it may be incomplete
-beat_times_to_include = beat_times[0:((len(beat_times) // meter) * meter)]
-reshaped = beat_times_to_include.reshape(-1, meter)
-bar_durations = (reshaped[1:] - reshaped[:-1])[:, 0]
-# just make the last bar same length as the penultimate this is ugly but covers both the situation when we have the
-# exact amount of beats to get only full bars and when there are additional beats
-bar_durations = np.append(bar_durations, bar_durations[-1])
-start_times = reshaped[:, 0]
+# TODO use other meters, ideally - detect
+METER = 4
 
 
-def convert_to_clave(start_times, durations):
+def convert_to_clave(bar_start_times, bar_durations):
     clave_pattern = np.array([0, 3 / 16, 6 / 16, 10 / 16, 12 / 16]).reshape(1, -1)
-    patterns = durations.reshape(-1, 1) * clave_pattern
-    return (patterns.T + start_times).T.reshape(-1, 1)
+    patterns = bar_durations.reshape(-1, 1) * clave_pattern
+    return (patterns.T + bar_start_times).T.reshape(-1, 1)
 
 
-clave_beats = convert_to_clave(start_times, bar_durations)
+def latinize(source_file_path):
+    x, sr = librosa.load(source_file_path)
+    proc = madmom.features.beats.DBNBeatTrackingProcessor(fps=100)
+    act = madmom.features.beats.RNNBeatProcessor()(source_file_path)
+    beat_times = proc(act)
+    # we need to skip the last bar as it may be incomplete
+    beat_times_to_include = beat_times[0:((len(beat_times) // METER) * METER)]
+    reshaped = beat_times_to_include.reshape(-1, METER)
+    durations = (reshaped[1:] - reshaped[:-1])[:, 0]
+    # just make the last bar same length as the penultimate.
+    # This is ugly but covers both the situation when we have the
+    # exact amount of beats to get only full bars and when there are additional beats
+    durations = np.append(durations, durations[-1])
+    start_times = reshaped[:, 0]
+    clave_beats = convert_to_clave(start_times, durations)
+    clicks = librosa.clicks(clave_beats, sr=sr, length=len(x))
+    data = x + clicks
+    sf.write(os.path.dirname(__file__) + '/latin_' + os.path.split(source_file_path)[-1], data, sr, subtype='PCM_24')
 
-clicks = librosa.clicks(clave_beats, sr=sr, length=len(x))
 
-data = x + clicks
-sf.write('latin_' + filename + '.wav', data, sr, subtype='PCM_24')
+if __name__ == "__main__":
+    file_path = sys.argv[1]
+    latinize(file_path)
